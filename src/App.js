7569b375066f805329be08a24c39502f8e64a7e2
Ian Scott <chesnekov@gmail.com>
2020-09-06 11:40:04 -0700

    remove some unecesary files, update App.js content with content from lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+    </React.Fragment>
   );
 }
 
