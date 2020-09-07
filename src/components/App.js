a0396109f23549f50c595beb368341d493cb358f
Ian Scott <chesnekov@gmail.com>
2020-09-07 10:50:44 -0700

    adds ticket.js component

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,10 @@ import React from "react";
 import Header from "./Header";
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
   return(
     <React.Fragment>
       <Header />
-      <h3>3a</h3>
-  <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
+      <TicketList />
     </React.Fragment>
   );
 }
