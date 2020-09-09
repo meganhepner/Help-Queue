c8ba8770313f8e296ed3d9df7331ae7e249a54d3
Ian Scott <chesnekov@gmail.com>
2020-09-09 09:56:26 -0700

    adds unidirectional data flow from TicketControler to newTicketForm. Allows for the creation of new tickets, and updates the master ticket control state with the change

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
 
-function newTicketFOrm(props){
+function newTicketForm(props){
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    console.log(event.target.names.value);
-    console.log(event.target.location.value);
-    console.log(event.target.issue.value);
+    props.onNewTicketCreation({ names: event.target.names.value,
+                              location: event.target.location.value,
+                              issue: event.target.issue.value,
+                              id: v4() 
+                              });
   }
   
   return (
@@ -29,5 +32,9 @@ function newTicketFOrm(props){
     </React.Fragment>
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default newTicketFOrm;
